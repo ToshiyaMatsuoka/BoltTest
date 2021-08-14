154fa92d2aa33f7fbe6580819b37f4acb6588c2b
ToshiyaMatsuoka <triger.1306@gmail.com>
2021-08-14 23:11:32 +0900

    色々追加

---
+++ Assets/PlayerCtrler.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+using Ludiq;
+
+public class PlayerCtrler : MonoBehaviour
+{
+    public FlowMachine flowMachine;
+    
+    public void Action()
+    {
+        Debug.Log("Actioné¿çs");
+    }
+}
+
+
